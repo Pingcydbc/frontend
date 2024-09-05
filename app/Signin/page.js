50a4f7b8bf4d067bdfd9bddefaaf53992651a38e
Pingcydbc <pingchayodom2323@gmail.com>
2024-09-05 10:41:21 +0700

    adada

--- app/Signin/page.js
+++ app/Signin/page.js
@@ -40,7 +40,7 @@ export default function Page() {
           showConfirmButton: false
         }).then(() => {
           // เปลี่ยนไปยังหน้า http://localhost:3001/ หลังจากล็อกอินสำเร็จ
-          window.location.href = '/Home';
+          window.location.href = '/';
         });
       } else {
         setMessage(result.error);
