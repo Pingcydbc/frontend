a16aece509d35554c22fe6938350d8af0b4a3e63
Pingcydbc <pingchayodom2323@gmail.com>
2024-09-05 10:44:38 +0700

    adadadadada

--- app/Signin/page.js
+++ app/Signin/page.js
@@ -38,7 +38,10 @@ export default function Page() {
           icon: 'success',
           timer: 1500, // แสดงการแจ้งเตือน 1.5 วินาที
           showConfirmButton: false
-        })
+        }).then(() => {
+          // เปลี่ยนไปยังหน้า http://localhost:3001/ หลังจากล็อกอินสำเร็จ
+          window.location.href = '';
+        });
       } else {
         setMessage(result.error);
         Swal.fire({
